759ad5282e152e4a3dd292678b662f283f3f7326
Jeff Marks <jeff.t.marks@gmail.com>
2022-11-03 13:57:37 -0400

    done with assignment

--- javascript/binary_search.js
+++ javascript/binary_search.js
@@ -1,31 +1,53 @@
 function binarySearch(arr, target) {
-  // type your code here
+  if (arr.length === 0) {
+		return false;
+	}
+
+	const middle = Math.floor(arr.length / 2);
+	const middle_value = arr[middle];
+
+	if (middle_value === target) {
+		return true;
+	} else if (middle_value > target) {
+		return binarySearch(arr.slice(0, middle), target);
+	} else {
+		return binarySearch(arr.slice(middle + 1), target);
+	}
 }
 
 // BONUS: MODIFY YOUR CODE TO RETURN THE INDEX OF THE TARGET, -1 IF NOT FOUND
 function binarySearchIndex(arr, target) {
-
+	  if (binarySearch(arr, target)) {
+			return arr.indexOf(target);
+		} else {
+			return -1;
+		}
 }
 
 if (require.main === module) {
   // add your own tests in here
-  console.log("Expecting: true");
-  console.log("=>", binarySearch([1, 2, 3], 3));
+  // console.log("Expecting: true");
+  // console.log("=>", binarySearch([1, 2, 3], 3));
+
+  // console.log("");
+
+  // console.log("Expecting: false");
+  // console.log("=>", binarySearch([3, 5, 9], 10));
+
+  // UNCOMMENT FOR BONUS
+  console.log("");
+  console.log("Expecting: 0");
+  console.log("=>", binarySearchIndex([1, 2, 3], 1));
 
   console.log("");
 
-  console.log("Expecting: false");
-  console.log("=>", binarySearch([3, 5, 9], 10));
+  console.log("Expecting: -1");
+  console.log("=>", binarySearchIndex([4, 7, 20], 100));
 
-  // UNCOMMENT FOR BONUS
-  // console.log("");
-  // console.log("Expecting: 0");
-  // console.log("=>", binarySearchIndex([1, 2, 3], 1));
+	console.log("");
 
-  // console.log("");
-
-  // console.log("Expecting: -1");
-  // console.log("=>", binarySearchIndex([4, 7, 20], 100));
+  console.log("Expecting: 4");
+  console.log("=>", binarySearchIndex([4, 7, 20, 100, 350, 400], 350));
 }
 
 module.exports = {
